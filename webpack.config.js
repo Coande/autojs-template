157497a23ebe8daa7a9f1f61f9b8f3c496b4ac42
Coande <e12e@qq.com>
2019-05-11 17:11:23 +0800

    添加 webpack 和 babel-loader

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            sourceMap: true
+          }
+        }
+      }
+    ]
+  }
+};
